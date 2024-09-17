926b9c588b5a3cb1ac4b80b96db80c9d88229c50
uncroos <any492266@gmail.com>
2024-09-17 18:40:17 +0900

    초기설정

---
+++ game/collision.py
@@ -0,0 +1,76 @@
+import os
+import pygame
+#########################################
+
+#pygame 기본 초기화
+pygame.init()
+
+#화면 크기 설정
+screen_width = 480 #가로
+screen_height = 632.18 #세로
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+#화면 타이틀 설정
+pygame.display.set_caption("고전 픽셀 게임")
+
+#FPS(Frame Per Second) 설정
+clock = pygame.time.Clock()
+
+#게임 초기화(배경화며나, 이미지, 좌표, 속도 등등)
+current_path = os.path.dirname(__file__) #현재 파일의 경로
+image_path = os.path.join(current_path, "images")
+
+#배경 이미지
+background = pygame.image.load(os.path.join(image_path, "background.png"))
+
+#스테이지 이지미
+stage = pygame.image.load(os.path.join(image_path, "stage.png"))
+stage_size = stage.get_rect().size
+stage_height = stage_size[1]
+
+#캐릭터 이미지
+character = pygame.image.load(os.path.join(image_path, "character.png"))
+character_size = character.get_rect().size
+character_width = character_size[0]
+character_height = character_size[1]
+character_x_pos = (screen_width - character_width) / 2
+character_y_pos = screen_height - character_height - stage_height
+
+#캐릭터 이동 방향
+character_to_x = 0
+
+#캐릭터 이동 속도
+character_speed = 5
+
+#무기 만들기
+weapon = pygame.image.load(os.path.join(image_path, "weapon.png"))
+weapon_size = weapon.get_rect().size
+weapon_width = weapon_size[0]
+
+#무기 발사 계수
+weapons = []
+
+#무기 이동 속도
+weapon_speed = 10
+
+# 공 만들기
+ball_images = [
+    pygame.image.load(os.path.join(image_path, "balloon1.png")),
+    pygame.image.load(os.path.join(image_path, "balloon2.png")),
+    pygame.image.load(os.path.join(image_path, "balloon3.png")),
+    pygame.image.load(os.path.join(image_path, "balloon4.png"))]
+
+# 공 크기에 따른 최초 스피드
+ball_speed_y = [-18, -15, -12, -9]
+
+# 공
+balls = []
+
+# 최초 발생하는 큰 공 추가
+balls.append({
+    "pos_x" : 50, # 공의 x 좌표
+    "pos_y" : 50, # 공의 y 좌표
+    "img_idx" : 0, # 공의 이미지 인덱스
+    "to_x": 3, # x축 이동방향, -3 이면 왼쪽으로, 3 이면 오른쪽으로
+    "to_y": -6, # y축 이동방향,
+    "init_spd_y": ball_speed_y[0]})# y 최초 속도
