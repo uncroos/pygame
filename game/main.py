9705d739ddbb6fa249a75c2ff9ab4a860d234fe8
uncroos <any492266@gmail.com>
2024-09-17 16:43:14 +0900

    타이틀 설정

--- game/main.py
+++ game/main.py
@@ -9,3 +9,6 @@ pygame.init()
 screen_width = 480 #가로
 screen_height = 632.18 #세로
 screen = pygame.display.set_mode((screen_width, screen_height))
+
+#화면 타이틀 설정
+pygame.display.set_caption("고전 픽셀 게임")
