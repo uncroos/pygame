b552eb4c07b44acf3def7b2e1a20e123756fed68
uncroos <any492266@gmail.com>
2024-09-17 18:42:53 +0900

    초기화

--- game/collision.py
+++ game/collision.py
@@ -78,3 +78,64 @@ balls.append({
 # 사라질 무기, 공 정보 저장 변수
 weapon_to_remove = -1
 ball_to_remove = -1
+
+running = True
+while running:
+    dt = clock.tick(30)
+    
+    #이벤트 처리
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False 
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT: # 캐릭터를 왼쪽으로
+                character_to_x -= character_speed
+            elif event.key == pygame.K_RIGHT: # 캐릭터를 오른쪽으로
+                character_to_x += character_speed
+            elif event.key == pygame.K_SPACE: # 무기 발사
+                weapon_x_pos = character_x_pos + (character_width / 2) - (weapon_width / 2)
+                weapon_y_pos = character_y_pos
+                weapons.append([weapon_x_pos, weapon_y_pos])
+        
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                character_to_x = 0
+
+    # 게임 캐릭터 위치 정의
+    character_x_pos += character_to_x
+
+    if character_x_pos < 0:
+        character_x_pos = 0
+    elif character_x_pos > screen_width - character_width:
+        character_x_pos = screen_width - character_width
+
+    # 무기 위치 조정
+    weapons = [[w[0], w[1] - weapon_speed] for w in weapons]
+
+    # 무기 없애기
+    weapons  = [[w[0], w[1]] for w in weapons if w[1] > 0]
+
+    # 공 위치 정의
+    for ball_idx, ball_val in enumerate(balls):
+        ball_pos_x = ball_val["pos_x"]
+        ball_pos_y = ball_val["pos_y"]
+        ball_img_idx = ball_val["img_idx"]
+
+        ball_size = ball_images[ball_img_idx].get_rect().size
+        ball_width = ball_size[0]
+        ball_height = ball_size[1]
+
+        # 가로벽에 닿았을 때 공 이동 위치 변경 (튕겨 나오는 효과)
+        if ball_pos_x < 0 or ball_pos_x > screen_width - ball_width:
+            ball_val["to_x"] = ball_val["to_x"] * -1
+
+        # 세로 위치
+        # 스테이지에 튕겨서 올라가는 처리
+        if ball_pos_y >= screen_height - stage_height - ball_height:
+            ball_val["to_y"] = ball_val["init_spd_y"]
+        else: # 그 외의 모든 경우에는 속도를 증가
+            ball_val["to_y"] += 0.5
+
+        ball_val["pos_x"] += ball_val["to_x"]
+        ball_val["pos_y"] += ball_val["to_y"]
